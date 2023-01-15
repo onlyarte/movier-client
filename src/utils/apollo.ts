08e347fa523bffde9d359545dc5e2475fc40d795
Ruslan Purii <ruslanpurii@gmail.com>
2023-01-15 19:14:12 +0100

    Init Apollo Client and Tailwind

---
+++ src/utils/apollo.ts
@@ -0,0 +1,11 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+export const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
