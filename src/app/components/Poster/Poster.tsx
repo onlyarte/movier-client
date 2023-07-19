2369e860a7d9ba0dceb59c84418542575ad8aee2
Ruslan Purii <ruslanpurii@gmail.com>
2023-07-19 20:35:03 +0200

    Upgrade images

--- src/app/components/Poster/Poster.tsx
+++ src/app/components/Poster/Poster.tsx
@@ -5,21 +5,22 @@ import Image, { ImageLoader, ImageProps } from 'next/image';
 // Supported values:
 // [ 'w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original' ]
 export default function Poster(props: Omit<ImageProps, 'loader'>) {
-  const posterLoader: ImageLoader = ({ src, width, quality }) => {
+  const posterLoader: ImageLoader = ({ src, width }) => {
+    let newSrc = src.replace('http://', 'https://');
     if (width < 92) {
-      return src.replace('original', 'w92');
+      newSrc = newSrc.replace('original', 'w92');
     } else if (width < 154) {
-      return src.replace('original', 'w154');
+      newSrc = newSrc.replace('original', 'w154');
     } else if (width < 185) {
-      return src.replace('original', 'w185');
+      newSrc = newSrc.replace('original', 'w185');
     } else if (width < 342) {
-      return src.replace('original', 'w342');
+      newSrc = newSrc.replace('original', 'w342');
     } else if (width < 500) {
-      return src.replace('original', 'w500');
+      newSrc = newSrc.replace('original', 'w500');
     } else if (width < 780) {
-      return src.replace('original', 'w780');
+      newSrc = newSrc.replace('original', 'w780');
     }
-    return src;
+    return newSrc;
   };
 
   return <Image {...props} loader={posterLoader} />;
